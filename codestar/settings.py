5ee62fa80f162c5022f12703dcdc31e2d8a7e51a
Andrea Nilsson <andreaycoral@icloud.com>
2024-11-03 08:39:11 +0000

    Add list view and filters

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("my_blog_is_awsome", 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-quetzaly88-djangoblog-1fse2o7kfuc.ws.codeinstitute-ide.net",
